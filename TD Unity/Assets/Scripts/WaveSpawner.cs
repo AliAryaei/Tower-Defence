462a6e8a49e4019a5e9bdc02b6d4ba40da7b122c
AliAryaei <ali-aryaei@outlook.com>
2023-02-06 07:35:19 +0100

    added map, scripts for waypoints, enemy and wave spawner

---
+++ TD Unity/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public float timeBetweenWaves = 5f;
+    public Transform spawnPoint;
+
+    private float countDown = 2f;
+    private int waveIndex = 1;
+
+    void Update()
+    {
+        if (countDown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }
+        countDown -= Time.deltaTime;
+    }
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5F);
+        }
+        Debug.Log("Wave incoming");
+        waveIndex++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab,spawnPoint.position,spawnPoint.rotation);
+
+    }
+}
